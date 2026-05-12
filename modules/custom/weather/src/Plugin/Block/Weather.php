adf4c3e7c0daaaf1a663a3e5f52dc3fc25c20bc5
Tom Wheeler <tom@wheelercreek.com>
2026-05-12 08:58:31 -0700

    changes to units

--- modules/custom/weather/src/Plugin/Block/Weather.php
+++ modules/custom/weather/src/Plugin/Block/Weather.php
@@ -34,37 +34,38 @@ class Weather extends BlockBase {
       $air_temp = $content->data->{'Air Temperature'}[0]->{'readings'}[0]->{'value'};
       $air_temp_F = $air_temp * 1.8 + 32;
       $atmospheric_pressure = $content->data->{'Atmospheric Pressure'}[0]->{'readings'}[0]->{'value'};
+      $atmospheric_pressure = round($atmospheric_pressure * 10, 2);
       $wind_gust = $content->data->{'Gust Speed'}[0]->{'readings'}[0]->{'value'};
-      $wind_gust = round($wind_gust * 2.23694, 2); // convert meters/second to miles/hour
+      $wind_gust = round($wind_gust * 1.94384, 2); // convert meters/second to knots
       $wind_dir = $content->data->{'Wind Direction'}[0]->{'readings'}[0]->{'value'};
       //build wind direction map
       if ($wind_dir >= 340 || $wind_dir < 20 ) {
-        $wind_dir = 'N';
+        $wind_dir_txt = 'N';
       }
       if ($wind_dir >= 20 && $wind_dir < 60 ) {
-        $wind_dir = 'NE';
+        $wind_dir_txt = 'NE';
       }
       if ($wind_dir >= 60 && $wind_dir < 110 ) {
-        $wind_dir = 'E';
+        $wind_dir_txt = 'E';
       }
       if ($wind_dir >= 110 && $wind_dir < 160 ) {
-        $wind_dir = 'SE';
+        $wind_dir_txt = 'SE';
       }
       if ($wind_dir >= 160 && $wind_dir < 200 ) {
-        $wind_dir = 'S';
+        $wind_dir_txt = 'S';
       }
       if ($wind_dir >= 200 && $wind_dir < 250 ) {
-        $wind_dir = 'SW';
+        $wind_dir_txt = 'SW';
       }
       if ($wind_dir >= 250 && $wind_dir < 290 ) {
-        $wind_dir = 'W';
+        $wind_dir_txt = 'W';
       }
       if ($wind_dir >= 290 && $wind_dir < 340 ) {
-        $wind_dir = 'NW';
+        $wind_dir_txt = 'NW';
       }
 
       $wind_speed = $content->data->{'Wind Speed'}[0]->{'readings'}[0]->{'value'};
-      $wind_speed = round($wind_speed * 2.23694, 2);  // convert meters/second to miles/hour
+      $wind_speed = round($wind_speed * 1.94384, 2);  // convert meters/second to knots
       $water_temp = $content->data->{'Water Temperature'}[0]->{'readings'}[0]->{'value'};
       $water_temp_F = $water_temp * 1.8 + 32;
       $precipitation = $content->data->{'Precipitation'}[0]->{'readings'}[0]->{'value'};
@@ -77,10 +78,10 @@ class Weather extends BlockBase {
       $output .= '<div class="weather_container"><h3>Current Conditions</h3>';
       $output .= '<div class="row"><span class="weather_label">Date & Time:</span> <span class="weather_val">' . $datetime . '</span></div>';
       $output .= '<div class="row"><span class="weather_label">Air Temperature:</span> <span class="weather_val">' . $air_temp_F. ' &#8457;</span></div>';
-      $output .= '<div class="row"><span class="weather_label">Atmospheric Pressure:</span> <span class="weather_val">' . $atmospheric_pressure. ' kPa</span></div>';
-      $output .= '<div class="row"><span class="weather_label">Wind Direction:</span> <span class="weather_val">' . $wind_dir. '</span></div>';
-      $output .= '<div class="row"><span class="weather_label">Wind Speed:</span> <span class="weather_val">' . $wind_speed. ' mph</span></div>';
-      $output .= '<div class="row"><span class="weather_label">Wind Gust:</span> <span class="weather_val">' . $wind_gust. ' mph</span></div>';
+      $output .= '<div class="row"><span class="weather_label">Atmospheric Pressure (PRES):</span> <span class="weather_val">' . $atmospheric_pressure. ' mbar</span></div>';
+      $output .= '<div class="row"><span class="weather_label">Wind Direction (WDIR):</span> <span class="weather_val">' . $wind_dir_txt . ' ('. $wind_dir. '&#176;)</span></div>';
+      $output .= '<div class="row"><span class="weather_label">Wind Speed (WSPD):</span> <span class="weather_val">' . $wind_speed. ' kts</span></div>';
+      $output .= '<div class="row"><span class="weather_label">Wind Gust (GST):</span> <span class="weather_val">' . $wind_gust. ' kts</span></div>';
       $output .= '<div class="row"><span class="weather_label">Water Temperature:</span> <span class="weather_val">' . $water_temp_F. ' &#8457;</span></div>';
       $output .= '<div class="row"><span class="weather_label">Water Level:</span> <span class="weather_val">' . $water_level. ' ft</span></div>';
       $output .= '<div class="row"><span class="weather_label">Precipitation:</span> <span class="weather_val">' . $precipitation. ' in</span></div>';
