cad6f73cd52c820e17de854757fb63d75f0e1636
Tom Wheeler <tom@wheelercreek.com>
2026-05-18 10:04:59 -0700

    caching on the block

--- modules/custom/weather/src/Plugin/Block/Weather.php
+++ modules/custom/weather/src/Plugin/Block/Weather.php
@@ -93,10 +93,11 @@ class Weather extends BlockBase {
       '#markup' => $output,
       '#attached' => ['library' => 'weather/drupal.weather'],
       '#allowed_tags' => ['a', 'div', 'span', 'button','h3','p', 'br'],
+      '#cache' => ['max-age' => 0],
     ];
   }
 
   public function getCacheMaxAge() {
-    return 1;
+    return 0;
   }
 }
